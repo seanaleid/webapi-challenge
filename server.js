59691ee03da3a54d9e5b8f07e292b87c25b242e1
seanaleid <seanaleid@gmail.com>
2019-11-01 11:56:15 -0400

    Removes DB files

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require('express');
 // importing helmet 
 const helmet = require('helmet');
 
+// server
 const server = express();
 
 // turning on json reading for express
 server.use(express.json());
 server.use(helmet());
 
+// set the routes here
+// server.use('/api/', router);
+
 // initial route to check server
 server.get('/', (req, res) => {
     res.status(200).json({ message: "The server is running 🤙"});
 });
 
-// set the routes here
-// server.use('/api/', router);
 
 module.exports = server;
